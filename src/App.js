badd2cf997acf3714677d6b1e573e34fe45edb90
Andrii Harnytskyi <a.harnytskyi@pnn.in.ua>
2023-12-09 15:02:01 +0200

    remove password credential

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
-/*global PasswordCredential */
-
-import logo from './logo.svg';
 import './App.css';
 
-function saveCredentials() {
-  console.log("saving started");
-  const credentials = new PasswordCredential({
-    id: document.getElementById("username").value,
-    password: document.getElementById("password").value,
-  });
-  navigator.credentials
-    .store(credentials)
-    .then(() => {
-      console.log('Credentials will store when you select "Save" in browser popup.');
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+function saveCredentials(){
+  console.log("credentials will be saved");
 }
 
-function retrieveCredentials() {
-  console.log("retrieving started");
-  navigator.credentials
-    .get({ password: true })
-    .then((credentials) => {
-      if (credentials) {
-        console.log(credentials);
-      } else {
-        console.log(
-          "No credentials found. Make sure you have saved credentials before trying to get them."
-        );
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+function retrieveCredentials(){
+  console.log("");
 }
 
 function App() {
