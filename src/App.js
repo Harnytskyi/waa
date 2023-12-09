95de7a5f19fc1cc9a090b792dd5f152da260943f
Andrii Harnytskyi <a.harnytskyi@pnn.in.ua>
2023-12-09 14:39:01 +0200

    init testing application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
+/*global PasswordCredential */
+
 import logo from './logo.svg';
 import './App.css';
+
+function saveCredentials() {
+  console.log("saving started");
+  const credentials = new PasswordCredential({
+    id: document.getElementById("username").value,
+    password: document.getElementById("password").value,
+  });
+  navigator.credentials
+    .store(credentials)
+    .then(() => {
+      console.log('Credentials will store when you select "Save" in browser popup.');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function retrieveCredentials() {
+  console.log("retrieving started");
+  navigator.credentials
+    .get({ password: true })
+    .then((credentials) => {
+      if (credentials) {
+        console.log(credentials);
+      } else {
+        console.log(
+          "No credentials found. Make sure you have saved credentials before trying to get them."
+        );
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" id="username" />
+      <input type="password" id="password" />
+      <button onClick={saveCredentials}>Save credentials</button>
+      <button onClick={retrieveCredentials}>Retrieve credentials</button>
     </div>
   );
 }
